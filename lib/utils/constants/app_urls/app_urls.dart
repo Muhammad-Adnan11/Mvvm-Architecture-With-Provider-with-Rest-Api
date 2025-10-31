9d9bbf70b3b4b942da88ed6757bd319431bd4c46
Muhammad Adnan <adnankhalil984@gmail.com>
2025-10-31 17:05:59 +0500

    network class add

---
+++ lib/utils/constants/app_urls/app_urls.dart
@@ -0,0 +1,8 @@
+class AppUrls {
+  static const String baseUrl = "https://api.example.com";
+
+  static const String loginUrl = "$baseUrl/auth/login";
+  static const String getProfileUrl = "$baseUrl/user/profile";
+  static const String updateProfileUrl = "$baseUrl/user/update";
+  static const String deleteUserUrl = "$baseUrl/user/delete";
+}
--- lib/view_model/getapiViewModel.dart
+++ lib/view_model/getapiViewModel.dart
@@ -1,24 +1,13 @@
 import "package:flutter/foundation.dart";
 import "package:mvvm_architecture_with_provider_with_rest_api/repository/getRepo.dart";
 
-
-
 class GetViewModel  with ChangeNotifier{
-
-
-
 final Getrepo repoService= Getrepo();
-
-
-
 Future<void> getItems ()async{
   try{
 final items= repoService.getITem("this is the URL");
-
-
   }catch(e){
     debugPrint(e.toString());
   }
 }
-
 }
